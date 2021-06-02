84b613f3b57e630e1a4284b48bfa116a736cf2e8
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-06-02 18:50:00 +0100

    Cleaned the code

--- Game.js
+++ Game.js
@@ -1,10 +1,9 @@
 class Game {
   constructor(start) {
-    
     this.stats = new Statistics();
     this.wallet = new Wallet(start);
 
-    document.getElementsById("start").addEventListener("click", this.startGame);
+    document.getElementById("start").addEventListener("click", this.startGame);
     this.spanWallet = document.querySelector(".panel span.wallet");
     this.boards = document.querySelectorAll("div.color");
     this.inputBid = document.getElementById("bid");
@@ -16,10 +15,8 @@ class Game {
     this.render();
   }
   render() {
-    console.log("Let's play!");
-    // this.spanWallet.textContent = 200;
+    this.spanWallet.textContent = 200;
   }
+
   startGame() {}
 }
-
-const game = new Game(200);
